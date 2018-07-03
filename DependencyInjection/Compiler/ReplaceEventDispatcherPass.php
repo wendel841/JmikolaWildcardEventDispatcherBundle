0111b40d2e0906155dae666c2334306d96b9512a
Ivan Popov <wendel841@gmail.com>
2018-07-03 19:23:35 +0300

    Update ReplaceEventDispatcherPass.php
    Symfony 4 compability

--- DependencyInjection/Compiler/ReplaceEventDispatcherPass.php
+++ DependencyInjection/Compiler/ReplaceEventDispatcherPass.php
@@ -4,12 +4,13 @@ namespace Jmikola\WildcardEventDispatcherBundle\DependencyInjection\Compiler;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 class ReplaceEventDispatcherPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container)
     {
-        $container->setDefinition('event_dispatcher', $container->getDefinition('jmikola_wildcard_event_dispatcher.event_dispatcher'));
+        $container->setDefinition(EventDispatcherInterface::class, $container->getDefinition('jmikola_wildcard_event_dispatcher.event_dispatcher'));
         $container->removeDefinition('jmikola_wildcard_event_dispatcher.event_dispatcher');
     }
 }
