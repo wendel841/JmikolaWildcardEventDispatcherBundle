3af14dba3c3cd293eb6a64e93e8b7de2819c30a8
Ivan Popov <wendel841@gmail.com>
2018-07-03 19:23:15 +0300

    Update SetInnerEventDispatcherPass.php
    Symfony 4 compability

--- DependencyInjection/Compiler/SetInnerEventDispatcherPass.php
+++ DependencyInjection/Compiler/SetInnerEventDispatcherPass.php
@@ -5,15 +5,16 @@ namespace Jmikola\WildcardEventDispatcherBundle\DependencyInjection\Compiler;
 use Symfony\Component\DependencyInjection\Alias;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 class SetInnerEventDispatcherPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container)
     {
-        if ($container->hasAlias('event_dispatcher')) {
-            $container->setAlias('jmikola_wildcard_event_dispatcher.event_dispatcher.inner', new Alias((string) $container->getAlias('event_dispatcher'), false));
+        if ($container->hasAlias(EventDispatcherInterface::class)) {
+            $container->setAlias('jmikola_wildcard_event_dispatcher.event_dispatcher.inner', new Alias((string) $container->getAlias(EventDispatcherInterface::class), false));
         } else {
-            $definition = $container->getDefinition('event_dispatcher');
+            $definition = $container->getDefinition(EventDispatcherInterface::class);
             $definition->setPublic(false);
             $container->setDefinition('jmikola_wildcard_event_dispatcher.event_dispatcher.inner', $definition);
         }
